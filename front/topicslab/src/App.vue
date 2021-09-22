cb4dd7950b6bf5921143d89825f305cdac05c301
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-09-22 09:26:25 +0900

    mypageをマイページに変更

--- front/topicslab/src/App.vue
+++ front/topicslab/src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">ホーム</router-link> |
     <template v-if="authenticated">
-      <a>mypage</a>
+      <a>マイページ</a>
     </template>
     <template v-else>
       <router-link to="/login">ログイン</router-link>
