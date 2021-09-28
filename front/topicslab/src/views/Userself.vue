480823b6548f78772e75c2e8a85795aba2742de1
Mai Mitarai <s1f101801891@iniad.org>
2021-09-28 11:58:57 +0900

    マイページのレイアウト変更()

--- front/topicslab/src/views/Userself.vue
+++ front/topicslab/src/views/Userself.vue
@@ -5,13 +5,13 @@
         マイページ
       </template>
       <template #content id="username">
-        {{user.name}}
-        <div v-if="isloading"><Skeleton width="30%" height="20px" class="p-mb-2"></Skeleton></div>
-        <div class="fields">
-          <label for="intro">自己紹介</label><br>
+        <div class="dummy_img">aaa</div>
+        <span class="u_name">{{user.name}}</span>
+        <div v-if="isloading"><Skeleton width="30%" height="25px" class="p-mb-2"></Skeleton></div><br>
+          <label class="textarea_des" for="intro">自己紹介</label><br>
           <InputText id="intro" type="textarea" v-model="intro" />
-          <Button label="更新" v-on:click="submitIntro" />
-        </div>
+          <Button id="intro_btn" label="更新" v-on:click="submitIntro" />
+        <div class="blank">a</div>
       </template>
       <template #footer>
         <Button label="トピック作成" v-on:click="toNewTopic" />
@@ -123,5 +123,37 @@ export default {
   .p-button {
     margin-right: 10px;
   }
+}
+/*画像の代わり*/
+.dummy_img{
+  float:left;
+  margin:0px 30px 0px 0px;
+  width:150px;
+  height:150px;
+  background-color:#aaa;
+}
+/*float解除用*/
+.blank{
+  clear: both;
+  color:#fff;
+  font-size:1px;
+}
+/*ユーザーの名前*/
+.u_name{
+  margin-bottom:100px;
+  font-size:25px;
+  font-weight:bold;
+}
+/*テキストエリアの説明*/
+.textarea_des{
+  font-size:13px;
+}
+/*テキストエリア*/
+#intro{
+  height:50px;
+}
+/*テキストエリアの更新ボタン*/
+#intro_btn{
+  height:50px;
 }
 </style>
