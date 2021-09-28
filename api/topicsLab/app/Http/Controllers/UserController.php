5f042b5702d856c93b0c2bdbff6b200bea53d00f
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-09-28 10:30:36 +0900

    ユーザの登録ができない問題を解決

--- api/topicsLab/app/Http/Controllers/UserController.php
+++ api/topicsLab/app/Http/Controllers/UserController.php
@@ -50,6 +50,7 @@ class UserController extends Controller
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
+        $user->intro = "";
         $user->save();
         return $user;
     }
