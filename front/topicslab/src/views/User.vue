26928c2848663320684d1492334b7c7aceec55b6
Mai Mitarai <s1f101801891@iniad.org>
2021-09-28 14:55:46 +0900

    ユーザーページのレイアウト変更

--- front/topicslab/src/views/User.vue
+++ front/topicslab/src/views/User.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <Card>
-      <template #header>
-          <img alt="user header" src="demo/images/usercard.png">
-      </template>
-      <template #title>
-          {{user.name}}のページ
-      </template>
+      <!--<template #title>
+          <Skeleton width="30%" height="30px" class="p-mb-2 skeleton_inline" v-if="isloading"></Skeleton>{{user.name}}のページ
+      </template>-->
       <template #content>
-        <div class="fields">
-          <label for="name">名前:</label><Skeleton width="20%" height="20px" class="p-mb-2 skeleton_inline" v-if="isloading"></Skeleton>{{user.name}}<br>
-          <label for="intro">自己紹介:</label><br>
-          <Skeleton height="20px" class="p-mb-2 skeleton_block" v-if="isloading"></Skeleton>
-          <Skeleton height="20px" class="p-mb-2" v-if="isloading"></Skeleton>
-          {{user.intro}}
-        </div>
+        <!--<img alt="user header" src="demo/images/usercard.png">-->
+        <div class="dummy_img">aaa</div>
+          <span class="u_name">{{user.name}}</span>
+          <Skeleton width="20%" height="30px" class="p-mb-2 skeleton_inline" v-if="isloading"></Skeleton><br>
+          <Skeleton width="63%" height="20px" class="p-mb-2 skeleton_block" v-if="isloading"></Skeleton>
+          <Skeleton width="63%" height="20px" class="p-mb-2 skeleton_block" v-if="isloading"></Skeleton>
+          <Skeleton width="63%" height="20px" class="p-mb-2 skeleton_block" v-if="isloading"></Skeleton>
+          <Skeleton width="63%" height="20px" class="p-mb-2" v-if="isloading"></Skeleton>
+          <div class="user_intro">
+            {{user.intro}}
+          </div>
+        <div class="blank">a</div>
       </template>
     </Card>
     <TabView>
@@ -88,6 +90,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+/**/
+Skeleton{
+  margin-left:0;
+}
 /*名前のダミー*/
 .skeleton_inline{
   display:inline-block;
@@ -96,5 +102,31 @@ export default {
 /*自己紹介のダミー(上段)*/
 .skeleton_block{
   margin-bottom: 5px;
+}
+
+/*画像の代わり*/
+.dummy_img{
+  float:left;
+  margin:0px 30px 0px 0px;
+  width:150px;
+  height:150px;
+  background-color:#aaa;
+}
+/*float解除用*/
+.blank{
+  clear: both;
+  color:#fff;
+  font-size:1px;
+}
+/*ユーザーの名前*/
+.u_name{
+  margin-bottom:100px;
+  font-size:25px;
+  font-weight:bold;
+}
+/**/
+.user_intro{
+  margin-top: 20px;
+  width:95%;
 }
 </style>
